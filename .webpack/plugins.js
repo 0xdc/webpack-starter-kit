39048e33892aabc0f4dfe8b8f0cf7f225cec6a9f
Daniel Cordero <webpack-starter-kit@0xdc.io>
2019-01-13 19:20:44 +0000

    Add html webpack plugin support

---
+++ .webpack/plugins.js
@@ -0,0 +1,5 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = [
+	new HTMLWebpackPlugin()
+]
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
-var rules = require('./.webpack/rules.js');
+var rules = require('./.webpack/rules.js'),
+    plugins = require('./.webpack/plugins.js');
 
 module.exports = {
 	mode: 'none',
 	module: {
 		rules: rules
-	}
+	},
+	plugins: plugins
 }
