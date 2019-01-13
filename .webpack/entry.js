173ffd5b7ace055e2c8379f3c12dfe793c931d63
Daniel Cordero <webpack-starter-kit@0xdc.io>
2019-01-13 20:13:29 +0000

    Add entrypoint for an express server

---
+++ .webpack/entry.js
@@ -0,0 +1,4 @@
+module.exports = {
+	main: './src/index.js',
+	server: './src/server.js'
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 var rules = require('./.webpack/rules.js'),
+    entry = require('./.webpack/entry.js'),
     plugins = require('./.webpack/plugins.js');
 
 module.exports = {
 	mode: 'none',
+	entry: entry,
 	module: {
 		rules: rules
 	},
